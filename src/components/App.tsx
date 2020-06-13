e73c557bed3c6509362c0496682290d8835dbdb2
Husein <nvdhunter007@gmail.com>
2020-06-13 17:21:15 +0700

    add delete functionality to the app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers";
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
 class _App extends React.Component<AppProps> {
+  onTodoClick = (id: number) => {
+    this.props.deleteTodo(id);
+  };
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
   };
 
   renderList = (): JSX.Element[] => {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
     });
   };
 
@@ -33,4 +42,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
