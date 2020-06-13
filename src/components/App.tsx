d9177979108b1af13e21c3f4bc65ff03cbd7b82d
Husein <nvdhunter007@gmail.com>
2020-06-13 16:10:16 +0700

    fetch on buttonClick, render the todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,13 +9,23 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
-  componentDidMount() {
+  onButtonClick = (): void => {
     this.props.fetchTodos();
-  }
+  };
+
+  renderList = (): JSX.Element[] => {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  };
 
   render() {
-    console.log(this.props.todos);
-    return <h1>Hi there</h1>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
